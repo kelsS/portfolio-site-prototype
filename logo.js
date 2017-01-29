558159a4515d0692ede62c4d76b4008267594d62
Kelsey S <kswanson426@gmail.com>
2017-01-29 15:14:32 -0500

    Create logo.js
    JS for logo menu functionality.

---
+++ logo.js
@@ -0,0 +1,19 @@
+var closeIcon = document.getElementById("close");
+closeIcon.style.visibility="hidden";
+
+
+
+//when menu is closed the open icon is displayed
+//while the menu is open the close icon displays
+function closeMenu(event) {
+	
+
+}
+
+
+//when menu is open the close icon dispays
+//while menu is closed the open icon displays
+function openMenu() {
+	document.getElementById("open");
+
+}
